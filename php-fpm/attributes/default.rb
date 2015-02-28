538efa07c9e57b0965e9e7b6ff39b96cb1037a83
kaimanainc <ms@kaimanamedia.com>
2015-02-28 12:58:04 +1000

    Update default.rb

--- php-fpm/attributes/default.rb
+++ php-fpm/attributes/default.rb
@@ -4,7 +4,7 @@ default['php-fpm']['pid'] = "/var/run/php-fpm/php-fpm.pid"
 default['php-fpm']['error_log'] =  "/var/log/php-fpm/error.log"
 default['php-fpm']['log_level'] = "notice"
 default['php-fpm']['emergency_restart_threshold'] = 1
-default['php-fpm']['emergency_restart_interval'] = "199m"
+default['php-fpm']['emergency_restart_interval'] = "1m"
 default['php-fpm']['process_control_timeout'] = 30
 
 
@@ -15,21 +15,9 @@ default['php-fpm']['pool']['www']['allowed_clients'] = ["127.0.0.1"]
 default['php-fpm']['pool']['www']['user'] = 'nginx'
 default['php-fpm']['pool']['www']['group'] = 'nginx'
 default['php-fpm']['pool']['www']['process_manager'] = "dynamic"
-default['php-fpm']['pool']['www']['max_children'] = 69
-default['php-fpm']['pool']['www']['start_servers'] = 12
-default['php-fpm']['pool']['www']['min_spare_servers'] = 49
-default['php-fpm']['pool']['www']['max_spare_servers'] = 29
-default['php-fpm']['pool']['www']['max_requests'] = 209
+default['php-fpm']['pool']['www']['max_children'] = 60
+default['php-fpm']['pool']['www']['start_servers'] = 10
+default['php-fpm']['pool']['www']['min_spare_servers'] = 10
+default['php-fpm']['pool']['www']['max_spare_servers'] = 40
+default['php-fpm']['pool']['www']['max_requests'] = 20
 default['php-fpm']['pool']['www']['request_terminate_timeout'] = "39m"
-
-
-default['php-fpm']['pool']['testpool']['listen'] = "127.0.0.1:9001"
-default['php-fpm']['pool']['testpool']['allowed_clients'] = ["127.0.0.1"]
-default['php-fpm']['pool']['testpool']['user'] = 'nginx'
-default['php-fpm']['pool']['testpool']['group'] = 'nginx'
-default['php-fpm']['pool']['testpool']['process_manager'] = "dynamic"
-default['php-fpm']['pool']['testpool']['max_children'] = 50
-default['php-fpm']['pool']['testpool']['start_servers'] = 5
-default['php-fpm']['pool']['testpool']['min_spare_servers'] = 5
-default['php-fpm']['pool']['testpool']['max_spare_servers'] = 35
-default['php-fpm']['pool']['testpool']['max_requests'] = 500
