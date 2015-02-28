e8f7de1dffd499b03aa235d0abbccfc273877e98
kaimanainc <ms@kaimanamedia.com>
2015-02-28 17:46:52 +1000

    Update restart.rb

--- nginx/recipes/restart.rb
+++ nginx/recipes/restart.rb
@@ -1,12 +1,5 @@
- template "/etc/nginx/sites-enabled/default" do
-    source "site.erb"
-  end
-  
-  
-
-  
-  service "nginx" do
-  service_name "nginx"
-  supports :start => true, :stop => true, :restart => true, :reload => true
-  action [ :restart ]
+service "nginx" do
+ service_name "nginx"
+ supports :start => true, :stop => true, :restart => true, :reload => true
+ action [ :restart ]
 end
