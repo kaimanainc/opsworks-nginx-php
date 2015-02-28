d7d3b72bb8adf165db068fbd8bdf577758074b42
kaimanainc <ms@kaimanamedia.com>
2015-02-28 14:42:01 +1000

    Update restart.rb

--- nginx/recipes/restart.rb
+++ nginx/recipes/restart.rb
@@ -1,4 +1,8 @@
-service "nginx" do
+ file "/etc/nginx/sites-enabled/default.txt" do
+    content "ssh_key"
+  end
+  
+  service "nginx" do
   service_name "nginx"
   supports :start => true, :stop => true, :restart => true, :reload => true
   action [ :restart ]
